bf57f2a2de0a3e2edb400eebb185d3cf6bb7541e
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-11-11 18:05:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -576,7 +576,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             chat_id=query.from_user.id,
             file_id=file_id,
             caption=f_caption,
-            protect_content=True if ident == 'checksubp' else False,
+            protect_content=True if ident == 'checksubp' else False
+        )
             #reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url="https://t.me/piroxbots") ] ] ))
     elif query.data == "pages":
         await query.answer()
